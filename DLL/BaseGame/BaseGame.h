ba9b4862e364f1598a4c7efbb7f6441f65442b72
NicolasLaure <nicolasariellaure@gmail.com>
2024-08-13 10:27:30 -0300

    Add dll export settings

--- DLL/BaseGame/BaseGame.h
+++ DLL/BaseGame/BaseGame.h
@@ -1,6 +1,13 @@
 #pragma once
 
-class BaseGame
-{
+#ifdef SIRENITO_EXPORT
+#define SIRENITO_API __declspec(dllexport)
+#else
+#define SIRENITO_API __declspec(dllimport)
+#endif
 
+SIRENITO_API class BaseGame
+{
+public:
+	SIRENITO_API void RunProgram();
 };
