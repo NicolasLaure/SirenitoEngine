3632dfb7c78bf03146e07abc51dfaf118184c452
Nicodragon25 <nicolasariellaure@gmail.com>
2024-08-19 21:18:50 -0300

    Add basic vertex buffer

--- DLL/BaseGame/BaseGame.cpp
+++ DLL/BaseGame/BaseGame.cpp
@@ -11,10 +11,22 @@ void BaseGame::RunProgram(int width, int height, const char* title)
 	if (!glfwInit())
 		return;
 
-	glewInit();
-
 	Window windowInstance = Window(width, height, title);
 	Renderer rendererInstance;
+
+	glewInit();
+
+	float positions[6] =
+	{
+		-0.5f,-0.5f,
+		0.0f, 0.5f,
+		0.5f,-0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(windowInstance.GetWindow()))
@@ -24,14 +36,15 @@ void BaseGame::RunProgram(int width, int height, const char* title)
 
 		glBegin(GL_TRIANGLES);
 
-		glColor3f(1, 0, 0);
-		glVertex2f(-0.5, -0.5);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glColor3f(1, 0, 0);
+		//glVertex2f(-0.5, -0.5);
 
-		glColor3f(0, 1, 0);
-		glVertex2f(0, 0.5);
+		//glColor3f(0, 1, 0);
+		//glVertex2f(0, 0.5);
 
-		glColor3f(0, 0, 1);
-		glVertex2f(0.5, -0.5);
+		//glColor3f(0, 0, 1);
+		//glVertex2f(0.5, -0.5);
 		glEnd();
 
 		/* Swap front and back buffers */
